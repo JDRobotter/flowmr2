21e06309573e6d00d8d9b0e3ce5e6a748be6864e
JD <jeandamien.brossillon@gmail.com>
2018-03-09 12:12:59 +0100

    wip

--- train.py
+++ train.py
@@ -3,17 +3,17 @@
 import tensorflow as tf
 import numpy as np
 import os,sys,glob
+import logging
+#logging.getLogger().setLevel(logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
     
     input_layer = tf.reshape(features["x"], [-1, 40*3, 30, 1])
 
-    print(labels)
-
     # convolutional layer #1
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
-        filters=32,
+        filters=8,
         kernel_size=[3,1],
         padding="same",
         activation=tf.nn.relu)
@@ -22,14 +22,14 @@ def cnn_model_fn(features, labels, mode):
     # convolutionnal layer #2
     conv2 = tf.layers.conv2d(
         inputs=pool1,
-        filters=64,
+        filters=8,
         kernel_size=[5,5],
         padding="same",
         activation=tf.nn.relu)
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
 
     # dense layer
-    pool2_flat = tf.reshape(pool2, [-1, 20*15*64])
+    pool2_flat = tf.reshape(pool2, [-1, 20*15*8])
     dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
     dropout = tf.layers.dropout(
@@ -73,8 +73,8 @@ def walk_concatenate(path,op):
         ls.append(op(v))
     return np.array(ls,dtype=np.float16)
 
-def train(classifier,logging_hook,directory):
-    print("[+] training on",directory)
+def train(directory):
+    print("[+] loading",directory)
     vimgs = walk_concatenate(
         os.path.join(directory,"img_*"),
         # transform 8bit color channels to 0-1 floats
@@ -85,17 +85,8 @@ def train(classifier,logging_hook,directory):
         # transform ascii chars to 0-1 values
         np.vectorize(lambda x: 0 if x==48 else 1))
 
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x":vimgs},
-        y=vkbs,
-        batch_size=100,
-        num_epochs=None,
-        shuffle=True)
-
-    classifier.train(
-        input_fn=train_input_fn,
-        steps=20000,
-        hooks=[logging_hook])
+    print(" -> ",len(vimgs),len(vkbs),"loaded")
+    return vimgs,vkbs
 
 def main():
     classifier = tf.estimator.Estimator(
@@ -103,9 +94,41 @@ def main():
 
     logging_hook = tf.train.LoggingTensorHook(
         tensors={"probabilities":"softmax_tensor"},
-        every_n_iter=50)
+        every_n_iter=1)
 
-    train(classifier,logging_hook,"session_2018_03_06__21_03_54")
+    vimgs,vkbs = train("session_2018_03_06__21_03_54")
+    
+    N = 100
+    vimgs_eval = vimgs[:N]
+    vimgs_train = vimgs[N:]
+    vkbs_eval = vkbs[:N]
+    vkbs_train = vkbs[N:]
+
+    while True:
+        if 1:
+            print("[+] training")
+            train_input_fn = tf.estimator.inputs.numpy_input_fn(
+                x={"x":vimgs_train},
+                y=vkbs_train,
+                batch_size=100,
+                num_epochs=None,
+                shuffle=True)
+
+            classifier.train(
+                input_fn=train_input_fn,
+                steps=50,
+                hooks=[logging_hook])
+
+        if 1:
+            print("[+] evaluating")
+            eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+                x={"x":vimgs_eval},
+                y=vkbs_eval,
+                num_epochs=1,
+                shuffle=False)
+            eval_results = classifier.evaluate(input_fn=eval_input_fn)
+            print(eval_results)
+
 
 if __name__ == '__main__':
     main()
